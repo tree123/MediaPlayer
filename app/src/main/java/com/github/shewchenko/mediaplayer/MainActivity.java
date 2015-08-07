b5cef7c5bddb4b37ef0adc8e25c460eecee08e43
Shewchenko <shevchenko.oleg5@gmail.com>
2015-08-08 02:54:17 +0300

    added PlayList

---
+++ app/src/main/java/com/github/shewchenko/mediaplayer/MainActivity.java
@@ -0,0 +1,76 @@
+package com.github.shewchenko.mediaplayer;
+
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lv;
+    String[] items;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lv = (ListView)findViewById(R.id.lvPlayList);
+
+        ArrayList<File> mySongs = findSongs(Environment.getExternalStorageDirectory());
+        items = new String[ mySongs.size() ];
+        for (int i = 0; i<mySongs.size(); i++){
+            // toast(mySongs.get(i).getName().toString());
+            items[i] = mySongs.get(i).getName().toString().replace(".mp3","").replace(".wav","");
+        }
+        ArrayAdapter<String> adp = new ArrayAdapter<String>(getApplicationContext(),R.layout.song_layout,R.id.textView,items);
+        lv.setAdapter(adp);
+    }
+    public ArrayList<File> findSongs(File root){
+        ArrayList<File> a1 = new ArrayList<File>();
+        File[] files =root.listFiles();
+        for (File singleFile : files){
+            if (singleFile.isDirectory() && !singleFile.isHidden()){
+                a1.addAll(findSongs(singleFile));
+            }else {
+                if (singleFile.getName().endsWith(".mp3") || singleFile.getName().endsWith(".wav")){
+                    a1.add(singleFile);
+                }
+            }
+        }
+        return a1;
+    }
+
+    public void toast(String text){
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
